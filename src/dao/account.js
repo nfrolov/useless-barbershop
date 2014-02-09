f76e75cb14be6b5a07bbeec611ed7c36edcbca93
Nikita Frolov <nikita@frolov.fi>
2014-02-09 17:59:20 +0200

    provide more information about account

--- src/dao/account.js
+++ src/dao/account.js
@@ -2,8 +2,22 @@ var when = require('when'),
     query = require('../util/query');
 
 exports.findByUsername = function (username) {
-    return query('SELECT * FROM account WHERE username = $1', [username])
+    var queryString = '' +
+        '    SELECT a.username, a.password, ' +
+        '           c.name AS client_name, ' +
+        '           w.name AS worker_name, w.role AS worker_role ' +
+        '      FROM account a ' +
+        ' LEFT JOIN client c ON c.username = a.username ' +
+        ' LEFT JOIN worker w ON w.username = a.username ' +
+        '     WHERE a.username = $1 ';
+    return query(queryString, [username])
         .then(function (users) {
-            return users[0];
+            var user = users[0];
+            return user && {
+                username: user.username,
+                password: user.password,
+                name: user.client_name || user.worker_name,
+                role: user.worker_role || 'client'
+            };
         });
 };
