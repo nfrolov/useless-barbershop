84775919017caa3388607322c738db0c34aee16f
Nikita Frolov <nikita@frolov.fi>
2014-02-16 22:32:44 +0200

    extended account dao

--- src/dao/account.js
+++ src/dao/account.js
@@ -21,3 +21,41 @@ exports.findByUsername = function (username) {
             };
         });
 };
+
+exports.insert = function (entity) {
+    var insertQuery =
+        ' INSERT INTO account ' +
+        ' VALUES ($1, $2) ';
+    var params = [entity.username, entity.password];
+
+    return query(insertQuery, params).then(function () {
+        return entity;
+    });
+};
+
+exports.update = function (username, entity) {
+    var updateQuery, params;
+
+    if (entity.password) {
+        updateQuery =
+            ' UPDATE account ' +
+            '    SET username = $1, password = $2 ' +
+            '  WHERE username = $3 ';
+        params = [entity.username, entity.password, username];
+    } else {
+        updateQuery =
+            ' UPDATE account ' +
+            '    SET username = $1 ' +
+            '  WHERE username = $2 ';
+        params = [entity.username, username];
+    }
+
+    return query(updateQuery, params).then(function () {
+        return entity;
+    });
+};
+
+exports.delete = function (username) {
+    var deleteQuery = ' DELETE FROM account WHERE username = $1 ';
+    return query(deleteQuery, [username]);
+};
