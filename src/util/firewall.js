82bf425d1e4a90200fe64e667cd79f1a9b6c2f1b
Nikita Frolov <nikita@frolov.fi>
2014-02-19 01:11:37 +0200

    updated firewall utility

--- src/util/firewall.js
+++ src/util/firewall.js
@@ -1,6 +1,9 @@
-module.exports = function firewall(role, cb) {
+module.exports = function firewall(roles, cb) {
+    if ('string' === typeof roles) {
+        roles = [roles];
+    }
     return function (req, res) {
-        if (req.user && role === req.user.role) {
+        if (req.user && -1 !== roles.indexOf(req.user.role)) {
             cb(req, res);
         } else {
             res.status(403).render('auth/denied');
